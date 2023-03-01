20baf55a14d409ce1af604cff15d2acf84c393bd
ModyQyW <wurui-dev@foxmail.com>
2023-03-01 13:57:10 +0800

    types: improve for `pages.json`

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -77,5 +77,9 @@ export interface PageMetaDatum {
    * 配置页面窗口表现，配置项参考下方 pageStyle
    */
   style?: GlobalStyle
+  /**
+   * 当前页面是否需要登录才可以访问，此配置优先级高于 uniIdRouter 下的 needLogin
+   */
+  needLogin?: boolean;
   [x: string]: any
 }
