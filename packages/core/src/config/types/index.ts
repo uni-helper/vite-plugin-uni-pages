3b342a835d025631ba03dafe67cf95ea7ec8497e
ModyQyW <wurui-dev@foxmail.com>
2023-03-01 12:18:54 +0800

    feat: 增加 uniIdRouter 和 entryPagePath

--- packages/core/src/config/types/index.ts
+++ packages/core/src/config/types/index.ts
@@ -43,6 +43,23 @@ export interface PreloadRule {
   network?: string
 }
 
+export interface UniIdRouter {
+  /**
+   * 登录页面路径
+   */
+  loginPage: string;
+  /**
+   * 需要登录才可以访问的页面列表，可以使用正则语法
+   */
+  needLogin: string[];
+  /**
+   * 是否自动解析云对象及 clientDB 的错误码，如果是客户端 token 不正确或 token 过期则自动跳转配置的登录页面
+   * 
+   * @default true
+   */
+  resToLogin?: boolean;
+}
+
 export interface SubPackage {
   /**
    * 子包的根目录
@@ -149,9 +166,14 @@ export interface PagesConfig {
   rightWindow?: TheWindow
 
   /**
-   * 自动跳转相关配置，新增于HBuilderX 3.5.0
+   * 自动跳转相关配置，新增于 HBuilderX 3.5.0
    */
-  uniIdRouter?: any
+  uniIdRouter?: UniIdRouter;
+
+  /**
+   * 默认启动首页，新增于 HBuilderX 3.7.0
+   */
+  entryPagePath?: string;
 
   [x: string]: any
 }
