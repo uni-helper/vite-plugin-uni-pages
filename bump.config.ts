b420ba4b0bc6f499c9d11757cbe61cc64b4ab410
KeJunMao <kejun1997@gmail.com>
2023-04-14 13:57:58 +0800

    chore: update packages.json

---
+++ bump.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  files: [
+    'package.json',
+    'packages/core/package.json',
+    'packages/playground/package.json',
+    'packages/schema/package.json',
+    'packages/volar/package.json',
+  ],
+})
