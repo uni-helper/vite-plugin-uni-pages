acd635f86c686cf66b17a2e3fd63d46cf9f7fd43
cnguu <www@cnguu.cn>
2025-08-19 18:03:34 +0800

    test: 补充测例

---
+++ packages/playground/src/pages/test-jsonc-with-comment.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>test</div>
+</template>
+
+<route lang="jsonc">
+{
+  "style": {
+    // #ifdef APP
+    "navigationBarTitleText": "test jsonc page APP"
+    // #endif
+  },
+  // #ifdef APP
+  "enablePullDownRefresh": true
+  // #endif
+}
+</route>
