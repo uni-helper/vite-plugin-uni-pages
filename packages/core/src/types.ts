c2e50ec169319d45b08fcc3c61720ac174896c8f
Sunpm <isunpm@qq.com>
2025-08-20 10:13:11 +0800

    feat: routeBlockLang 新增 'jsonc' 类型和文档说明

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -69,7 +69,7 @@ export interface Options {
    * Set the default route block parser, or use `<route lang="xxx">` in SFC route block
    * @default 'json5'
    */
-  routeBlockLang: 'json5' | 'json' | 'yaml' | 'yml'
+  routeBlockLang: 'json5' | 'jsonc' | 'json' | 'yaml' | 'yml'
 
   /**
    * minify the `pages.json`
