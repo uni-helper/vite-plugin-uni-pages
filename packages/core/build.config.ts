fa650c28ea5962398ed1d1f0dcc1fedd47a6becd
FliPPeDround <734243792@qq.com>
2025-08-27 11:25:06 +0800

    fix: esm error

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -1,11 +1,9 @@
-import process from 'node:process'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: ['src/index'],
   declaration: true,
   clean: true,
-  sourcemap: process.env.NODE_ENV === 'development',
   rollup: {
     emitCJS: true,
     inlineDependencies: true,
