696ae27185759ec44f21d9f04e17b9943bf9d6c3
edwinhuish <edwinhuish@gmail.com>
2025-09-23 23:43:36 +0800

    perf: 移除子进程重启的代码，将文件操作改为异步，并加锁

--- packages/core/src/files.ts
+++ packages/core/src/files.ts
@@ -1,6 +1,5 @@
 import type { ResolvedOptions } from './types'
 import fs from 'node:fs'
-import { stringify as cjStringify } from 'comment-json'
 import fg from 'fast-glob'
 
 import { FILE_EXTENSIONS } from './constant'
@@ -23,23 +22,46 @@ export function getPageFiles(path: string, options: ResolvedOptions): string[] {
   return files
 }
 
-export function checkPagesJsonFile(path: string) {
-  if (!fs.existsSync(path)) {
-    writeFileSync(path, cjStringify({ pages: [{ path: '' }] }, null, 2))
-    return false
+/**
+ * 检查指定路径的pages.json文件，如果文件不存在或不是有效文件则创建一个空的pages.json文件
+ * @param path - 要检查的文件路径
+ * @returns Promise<void> - 无返回值的异步函数
+ */
+export async function checkPagesJsonFile(path: fs.PathLike): Promise<boolean> {
+  const createEmptyFile = (path: fs.PathLike) => {
+    return fs.promises.writeFile(path, JSON.stringify({ pages: [{ path: '' }] }, null, 2), { encoding: 'utf-8' }).then(() => true).catch(() => false)
   }
-  return true
-}
 
-export function readFileSync(path: string) {
+  const unlink = (path: fs.PathLike) => {
+    return fs.promises.unlink(path).then(() => true).catch(() => false)
+  }
+
   try {
-    return fs.readFileSync(path, { encoding: 'utf-8' })
+    const stat = await fs.promises.stat(path)
+
+    if (!stat.isFile()) {
+      // 不是文件，尝试删除
+      if (!await unlink(path)) {
+        return false
+      }
+
+      return createEmptyFile(path) // 创建空文件
+    }
+    // 是文件
+
+    const access = await fs.promises.access(path, fs.constants.W_OK | fs.constants.R_OK).then(() => true).catch(() => false)
+    if (!access) {
+      // 无权限，尝试删除
+      if (!await unlink(path)) {
+        return false
+      }
+
+      return createEmptyFile(path) // 创建空文件
+    }
+    return true
   }
   catch {
-    return ''
+    // stat 出错，证明没此文件
+    return createEmptyFile(path) // 创建空文件
   }
-}
-
-export function writeFileSync(path: string, content: string) {
-  fs.writeFileSync(path, content, { encoding: 'utf-8' })
 }
