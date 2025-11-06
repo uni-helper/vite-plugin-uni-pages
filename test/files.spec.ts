bf0dd5178af36ce5e7dc9dbe1ebc0999d730148c
feige996 <1020102647@qq.com>
2025-11-06 09:15:30 +0800

    test(core): 更新文件测试用例以匹配新的目录结构

--- test/files.spec.ts
+++ test/files.spec.ts
@@ -2,33 +2,58 @@ import process from 'node:process'
 import { describe, expect, it } from 'vitest'
 import { getPageFiles, resolveOptions } from '../packages/core/src'
 
-const options = resolveOptions({}, process.cwd())
-const pages = 'packages/playground/src/pages'
+const pages = 'packages/playground/src'
 
-describe('get files', () => {
+const options = resolveOptions({}, process.cwd())
+describe('get all files', () => {
   it('pages', async () => {
     const files = getPageFiles(pages, options)
     expect(files.sort()).toMatchInlineSnapshot(`
-      [
-        "A-top.vue",
-        "blog/index.vue",
-        "blog/post.vue",
-        "define-page/async-function.vue",
-        "define-page/conditional-compilation.vue",
-        "define-page/function.vue",
-        "define-page/nested-function.vue",
-        "define-page/object.vue",
-        "define-page/option-api.vue",
-        "define-page/remove-console.vue",
-        "define-page/yaml.vue",
-        "i18n.vue",
-        "index.nvue",
-        "index.vue",
-        "test-json.vue",
-        "test-jsonc-with-comment.vue",
-        "test-yaml.vue",
-        "test.vue",
-      ]
+[
+  "App.vue",
+  "components/Counter.vue",
+  "pages-sub-pages/sub-activity/pages/about/index.vue",
+  "pages-sub-pages/sub-activity/pages/home/index.vue",
+  "pages-sub-pages/sub-main/pages/about/index.nvue",
+  "pages-sub-pages/sub-main/pages/about/index.vue",
+  "pages-sub-pages/sub-main/pages/home/index.vue",
+  "pages-sub/about/components/comp.vue",
+  "pages-sub/about/components/dir/nest-comp.vue",
+  "pages-sub/about/index.vue",
+  "pages-sub/about/your.vue",
+  "pages-sub/components/comp.vue",
+  "pages-sub/components/dir/nest-comp.vue",
+  "pages-sub/index.vue",
+  "pages-sub2/about/components/comp.vue",
+  "pages-sub2/about/components/dir/nest-comp.vue",
+  "pages-sub2/about/index.vue",
+  "pages-sub2/about/your.vue",
+  "pages-sub2/components/comp.vue",
+  "pages-sub2/components/dir/nest-comp.vue",
+  "pages-sub2/index.vue",
+  "pages/A-top.vue",
+  "pages/blog/components/comp.vue",
+  "pages/blog/components/dir/nest-comp.vue",
+  "pages/blog/index.vue",
+  "pages/blog/post.vue",
+  "pages/components/comp.vue",
+  "pages/components/dir/nest-comp.vue",
+  "pages/define-page/async-function.vue",
+  "pages/define-page/conditional-compilation.vue",
+  "pages/define-page/function.vue",
+  "pages/define-page/nested-function.vue",
+  "pages/define-page/object.vue",
+  "pages/define-page/option-api.vue",
+  "pages/define-page/remove-console.vue",
+  "pages/define-page/yaml.vue",
+  "pages/i18n.vue",
+  "pages/index.nvue",
+  "pages/index.vue",
+  "pages/test-json.vue",
+  "pages/test-jsonc-with-comment.vue",
+  "pages/test-yaml.vue",
+  "pages/test.vue",
+]
     `)
   })
 })
