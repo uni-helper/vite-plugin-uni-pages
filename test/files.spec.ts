735e515327d56d362bf9440f886fbb26802e9ae8
feige996 <1020102647@qq.com>
2025-11-06 09:22:06 +0800

    test(files): 添加排除组件目录的测试用例
    添加三个测试用例，分别测试：
    1. 排除所有 pages 下的 components 目录
    2. 排除 pages 下的 components 目录但保留 pages-sub2 下的 components
    3. 基本文件获取功能

--- test/files.spec.ts
+++ test/files.spec.ts
@@ -57,3 +57,101 @@ describe('get all files', () => {
     `)
   })
 })
+
+const options2 = resolveOptions({
+  exclude: ['pages*/**/components/**/*.*'],
+}, process.cwd())
+describe('get all files except components', () => {
+  it('pages2', async () => {
+    const files = getPageFiles(pages, options2)
+    expect(files.sort()).toMatchInlineSnapshot(`
+[
+  "App.vue",
+  "components/Counter.vue",
+  "pages-sub-pages/sub-activity/pages/about/index.vue",
+  "pages-sub-pages/sub-activity/pages/home/index.vue",
+  "pages-sub-pages/sub-main/pages/about/index.nvue",
+  "pages-sub-pages/sub-main/pages/about/index.vue",
+  "pages-sub-pages/sub-main/pages/home/index.vue",
+  "pages-sub/about/index.vue",
+  "pages-sub/about/your.vue",
+  "pages-sub/index.vue",
+  "pages-sub2/about/index.vue",
+  "pages-sub2/about/your.vue",
+  "pages-sub2/index.vue",
+  "pages/A-top.vue",
+  "pages/blog/index.vue",
+  "pages/blog/post.vue",
+  "pages/define-page/async-function.vue",
+  "pages/define-page/conditional-compilation.vue",
+  "pages/define-page/function.vue",
+  "pages/define-page/nested-function.vue",
+  "pages/define-page/object.vue",
+  "pages/define-page/option-api.vue",
+  "pages/define-page/remove-console.vue",
+  "pages/define-page/yaml.vue",
+  "pages/i18n.vue",
+  "pages/index.nvue",
+  "pages/index.vue",
+  "pages/test-json.vue",
+  "pages/test-jsonc-with-comment.vue",
+  "pages/test-yaml.vue",
+  "pages/test.vue",
+]
+    `)
+  })
+})
+
+const options3 = resolveOptions({
+  exclude: [
+    // 排除所有pages 里面的 components 文件夹
+    'pages*/**/components/**/*.*',
+    // 不排除 pages-sub2/**/components
+    '!pages-sub2/**/components/**/*.*',
+  ],
+}, process.cwd())
+
+describe('get all files except components but not pages-sub2/**/components', () => {
+  it('pages3', async () => {
+    const files = getPageFiles(pages, options3)
+    expect(files.sort()).toMatchInlineSnapshot(`
+[
+  "App.vue",
+  "components/Counter.vue",
+  "pages-sub-pages/sub-activity/pages/about/index.vue",
+  "pages-sub-pages/sub-activity/pages/home/index.vue",
+  "pages-sub-pages/sub-main/pages/about/index.nvue",
+  "pages-sub-pages/sub-main/pages/about/index.vue",
+  "pages-sub-pages/sub-main/pages/home/index.vue",
+  "pages-sub/about/index.vue",
+  "pages-sub/about/your.vue",
+  "pages-sub/index.vue",
+  "pages-sub2/about/components/comp.vue",
+  "pages-sub2/about/components/dir/nest-comp.vue",
+  "pages-sub2/about/index.vue",
+  "pages-sub2/about/your.vue",
+  "pages-sub2/components/comp.vue",
+  "pages-sub2/components/dir/nest-comp.vue",
+  "pages-sub2/index.vue",
+  "pages/A-top.vue",
+  "pages/blog/index.vue",
+  "pages/blog/post.vue",
+  "pages/define-page/async-function.vue",
+  "pages/define-page/conditional-compilation.vue",
+  "pages/define-page/function.vue",
+  "pages/define-page/nested-function.vue",
+  "pages/define-page/object.vue",
+  "pages/define-page/option-api.vue",
+  "pages/define-page/remove-console.vue",
+  "pages/define-page/yaml.vue",
+  "pages/i18n.vue",
+  "pages/index.nvue",
+  "pages/index.vue",
+  "pages/test-json.vue",
+  "pages/test-jsonc-with-comment.vue",
+  "pages/test-yaml.vue",
+  "pages/test.vue",
+]
+    `)
+  })
+})
