5abab3311b8aedf77250cf0c186b6f1eff650ecc
xiaohe0601 <xiaohe0601@outlook.com>
2025-11-13 17:45:48 +0800

    fix(types): 修复未启用 dts 时 virtual:uni-pages 类型错误 (#263)

--- packages/core/client.d.ts
+++ packages/core/client.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="./global.d.ts" />
+
 declare module 'virtual:uni-pages' {
   import type { SubPackage } from './src/config/types/index'
   import type { PageMetaDatum } from './src/types'
@@ -5,9 +7,3 @@ declare module 'virtual:uni-pages' {
   export const pages: PageMetaDatum[]
   export const subPackages: SubPackage[]
 }
-
-declare global {
-  const definePage: import('.').DefinePage
-}
-
-export {}
